ffb04fa66c7c88461cfb222a817e58b347387649
Aly Elaswad <124714695+alyelaswad@users.noreply.github.com>
2024-05-20 08:23:46 +0300

    Added some comments

--- Vivado/Modules/digitalClock.v
+++ Vivado/Modules/digitalClock.v
@@ -1,68 +1,66 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/02/2024 04:12:43 PM
-// Design Name: 
-// Module Name: digitalClock
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+/*******************************************************************
+*
+* Module: digitalClock.v
+* Project: Alarm_Clock
+* Author: Islam  islamemara@aucegypt.edu
+          Aly    alyelaswad@aucegypt.edu
+          Ismail ismailsabry@aucegypt.edu
+* Description: The main module that conncets the modules: mux4x1, minSecCount, pushButtonDetector, clockDivider and SevenSegDecWithEn
+*
+* Change history: 05/13/24 – built the module, added the MUX, added the clockdivider, added the MUX and added the minSecCount  
+*                 05/15/24 – Changed the Pushbutton clock, adjsuted the decimal point and adjusted the LEDs   
+*                 05/16/24 - Fixed an issue in the decimal point, adjsuted Buzzer output and fixed issues in the different clocks used 
+**********************************************************************/
 
 
-module digitalClock(input clk, rst, en,C, L , R , U , D ,
-output [6:0] segments,output  [3:0] anode_active,output reg dp,output LD0,LD9,LD12,LD13,LD14,LD15);
+module digitalClock(
+input clk, rst, en,C,L,R,U,D, // Inputs for the clock, reset, enable and the buttons
+output [6:0] segments, [3:0] anode_active, // Output for the segments display and anode active
+output LD0,buzzer,LD12,LD13,LD14,LD15,  // Output for the LEDs 
+output reg dp); // Output for the decimal point
 
-wire [13:0]  count; 
-wire outC,outD,outU,outL,outR; 
-clockDivider #(500000)div2(clk, rst, clk_buttons);
-//clockDivider div3(clk, rst, clk_buttonsU);
+wire dp1; // wire to disable the decimal point in adjust mode
+wire [13:0]  count; // The count output of the hour and minutes counters of the clock OR the output of the hour minutes of the alarm
+wire outC,outD,outU,outL,outR; // The output of the pushbuttons
+clockDivider #(500000)div2(clk, rst, clk_buttons); // The clockdivider for the clock of the pushbuttons
 
-pushButtonDetector C_btn (clk_buttons,rst, C , outC);
-pushButtonDetector D_btn(clk_buttons,rst, D, outD);
-pushButtonDetector U_btn(clk_buttons,rst, U, outU);
-pushButtonDetector L_btn(clk_buttons,rst, L, outL);
-pushButtonDetector R_btn(clk_buttons,rst, R, outR);
+pushButtonDetector C_btn (clk_buttons,rst, C , outC); // The center pushbutton
+pushButtonDetector D_btn(clk_buttons,rst, D, outD);  // The Down pushbutton
+pushButtonDetector U_btn(clk_buttons,rst, U, outU);  // The Up pushbutton
+pushButtonDetector L_btn(clk_buttons,rst, L, outL);  // The left pushbutton
+pushButtonDetector R_btn(clk_buttons,rst, R, outR);  // The right pushbutton
  
  
 minSecCount mrClock( .clk(clk),.clkBTN(clk_buttons), .rst(rst), .en(en),.C(outC),.L(outL),.R(outR),.U(outU),.D(outD), .MUX_count(count),
-.LD0(LD0),.LD12(LD12),.LD13(LD13),.LD14(LD14),.LD9(LD9),.LD15(LD15));
-wire [3:0] f4 = count[3:0];
-wire [2:0] t3 = count[6:4];
-wire [3:0] s2 = count[10:7];
-wire [2:0] f1 = count[13:11];
-wire [1:0] s;
-wire[3:0] muxOut;
-wire clk_out,clk_out2,clk_buttons,clk_buttonsU;
+.LD0(LD0),.LD12(LD12),.LD13(LD13),.LD14(LD14),.buzzer(buzzer),.LD15(LD15),.dp1(dp1)); // The module that otput the count of the clock and the alarm
+wire [3:0] f4 = count[3:0]; // wires used to put the count in the MUX 
+wire [2:0] t3 = count[6:4]; // wires used to put the count in the MUX
+wire [3:0] s2 = count[10:7]; // wires used to put the count in the MUX
+wire [2:0] f1 = count[13:11]; // wires used to put the count in the MUX
+wire [1:0] s; // Wire for the 2 bit binary counter used for the MUX and the 2x4 decoder 
+wire[3:0] muxOut; // Wire for the multiplexer output 
+wire clk_out, // clock for the 2 bit binary counter 
+clk_out2 // clock for the decimal point 
+,clk_buttons; // clock for the push buttons  
 
 
-clockDivider #(250000)div(clk, rst, clk_out);
-counter_x_bit #(2,4) select(.clk(clk_out), .reset(rst),.en(en), .count(s));
+clockDivider #(250000)div(clk, rst, clk_out); // The clockdivider for the clock of the 2 bit binary counter
+counter_x_bit #(2,4) select(.clk(clk_out), .reset(rst),.en(en), .count(s)); // 2 bit binary counter used for the MUX and the 2x4 decoder 
 
-mux4x1 Mux(.data_in_0(f4),.data_in_1({1'b0,t3}),.data_in_2(s2),.data_in_3({1'b0,f1}),.sel(s), .data_out(muxOut));
+mux4x1 Mux(.data_in_0(f4),.data_in_1({1'b0,t3}),.data_in_2(s2),.data_in_3({1'b0,f1}),.sel(s), .data_out(muxOut)); // The MUX for the multiplexed display of the clock
 
-SevenSegDecWithEn segf4(.en(s),.in(muxOut),.segments(segments), .anode_active(anode_active));
-
-
+SevenSegDecWithEn segf4(.en(s),.in(muxOut),.segments(segments), .anode_active(anode_active)); // Seven segment module for the display of the alarm and the clock
 
 
-clockDivider #(50000000)DUUT(.clk(clk), .rst(rst), .clk_out(clk_out2));
-always @(*)
+
+
+clockDivider #(50000000)DUUT(.clk(clk), .rst(rst), .clk_out(clk_out2)); // The clockdivider for the clock of the decimal point
+always @(*) // Assigning the the decimal point with the clock in clock mode and assigining it with 0 in the adjust mode
 begin
-if(anode_active==4'b1101 && clk_out2) 
- dp =0;
- else dp=1;
-
+if(anode_active==4'b1101&& dp1) 
+dp= clk_out2;   
+else  dp= 1; 
 end
 
 endmodule
--- Vivado/Modules/minSecCount.v
+++ Vivado/Modules/minSecCount.v
@@ -1,187 +1,204 @@
 
 //////////////////////////////////////////////////////////////////////////////////
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/02/2024 03:36:25 PM
-// Design Name: 
-// Module Name: minSecCount
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+/*******************************************************************
+*
+* Module: minSecCount.v
+* Project: Alarm_Clock
+* Author: Islam  islamemara@aucegypt.edu
+          Aly    alyelaswad@aucegypt.edu
+          Ismail ismailsabry@aucegypt.edu
+* Description: The module that outputs the count for the alarm, count for the clock, the LED signals and the buzzer signal
+*
+* Change history: 05/13/24 - Built the module and added the hour units and counter hour tens counter
+*                 05/15/24 - Adjusted the states logic by using 5 states (the states: clock, time hour, time minute, alarm hour, and the alarm minute),
+                  added the alarm counters, adjusted different clocks and added a MUX for the alarm and clock count.    
+*                 05/16/24 - Fixed major errors in the buzzer, fixed errors in the decimal point, added the alarm state to disable the alarm upon pressing any button  
+**********************************************************************/
 
-module minSecCount(input clk, clkBTN, rst, en, C,L,R,U,D, output [13:0] MUX_count,output reg LD0,LD9,LD12,LD13,LD14,LD15);
-//[13:0] A_count,
+module minSecCount(
+input clk, clkBTN, rst, en, C,L,R,U,D, // input for the regular clock, the clock of buttons, reset, enable and buttons 
+output [13:0] MUX_count, // The count outout: either the alarm or the clock
+output reg LD0,buzzer,LD12,LD13,LD14,LD15,dp1); // output of the LEDs and the decimal point 
 
- clockDivider #(50000000)div(clk, rst, clk_out1HZ);
-reg visited=0;
-reg clicked;
-wire [3:0] secount;
-wire [2:0] secount2;
-wire MUXclk;
-reg adj;
-wire [13:0] count;
-wire [13:0] A_count;
-wire [13:0] MUX_count;
-//assign MUXclk = (state!=CLK)? (U|D): clk;
-//wire [5:0] time_minutes = count[5:0];
-//wire [2:0] t3 = count[6:4];
-//wire [3:0] s2 = count[10:7];
-//wire [2:0] f1 = count[13:11];
+clockDivider #(50000000)div(clk, rst, clk_out1HZ); // Clock divider for the 1 Hz clock   
+reg visited=0; // register to check if the alarm visited  
+wire [3:0] secount; // count for the seconds units
+wire [2:0] secount2; // count for the seconds tens
+wire [13:0] count; // Count for the Time hour and minutes
+wire [13:0] A_count; // Count for the Alarm hour and minutes
+wire [13:0] MUX_count; // Count for either the Time hour and minutes or Alarm hour and minutes
 
 
 
-reg [2:0] state, nextState; 
+
+reg [2:0] state, nextState; // Registers for the current and next states
 parameter [2:0] CLK= 3'b000, TH = 3'b001, TM = 3'b010, AH=3'b011, AM=3'b100, AS=3'b101; // States Encoding
 always @ (*) begin
-case (state)
+case (state) // Combinational logic for the states stage
+
 CLK: if ({R,L,C}==3'b001) nextState = TH;
-    else if (!visited & count==A_count) nextState= AS;
- else nextState = CLK;
+     else if (!visited & count==A_count) nextState= AS;
+     else nextState = CLK;
  
 TH: if ({R,L,C}==3'b100) nextState = TM;
- else if ({R,L,C}==3'b010) nextState = AM;
- else if ({R,L,C}==3'b001) nextState = CLK;
- else nextState =TH;
+    else if ({R,L,C}==3'b010) nextState = AM;
+    else if ({R,L,C}==3'b001) nextState = CLK;
+    else nextState =TH;
  
 TM: if ({R,L,C}==3'b100) nextState = AH;
- else if ({R,L,C}==3'b010) nextState = TH;
- else if ({R,L,C}==3'b001) nextState = CLK;
- else nextState =TM;
+    else if ({R,L,C}==3'b010) nextState = TH;
+    else if ({R,L,C}==3'b001) nextState = CLK;
+    else nextState =TM;
  
 AH: if ({R,L,C}==3'b100) nextState = AM;
- else if ({R,L,C}==3'b010) nextState = TM;
- else if ({R,L,C}==3'b001) nextState = CLK;
- else nextState =AH;
+    else if ({R,L,C}==3'b010) nextState = TM;
+    else if ({R,L,C}==3'b001) nextState = CLK;
+    else nextState =AH;
  
 AM: if ({R,L,C}==3'b100) nextState = TH;
- else if ({R,L,C}==3'b010) nextState = AH;
- else if ({R,L,C}==3'b001) nextState = CLK;
- else nextState =AM;
-//default: nextState= CLK;
-AS:if(U|D|R|L|C) nextState=CLK;
-else nextState =AS;
+    else if ({R,L,C}==3'b010) nextState = AH;
+    else if ({R,L,C}==3'b001) nextState = CLK;
+    else nextState =AM;
+
+AS: if(U|D|R|L|C) nextState=CLK;
+    else if(count!=A_count)  nextState=CLK;
+    else nextState =AS;
+
 endcase
 end
-always @ (posedge clkBTN or  posedge rst) begin
-if(rst)
-state <= CLK;
-else
-state <= nextState;
-end
-reg ENC,ENTH,ENTM,ENAM,ENAH;
-reg clkAdjst;
-//wire clk_out5;
-// clockDivider #(50000000)div33(clk, rst, clk_out5);
-reg btnPressed,BTN;
-wire dec;
-always @(*)
-begin
-BTN = (state==CLK&A_count==count&(U|D|R|L|C));
-if(state==CLK)
-    begin
-        {LD12,LD13,LD14,LD15}=5'b0000;
-        ENC=en;
-        ENTH=0;
-        ENTM=0;
-        ENAM=0;
-        ENAH=0;
-         LD0 = 0;
-         LD9 = 0;
-//        if(!clicked)
-//       begin
-//        LD0 = (A_count == count) ? clk_out1HZ : 1'b0;
-//         LD9 = (A_count == count) ? clk_out1HZ : 1'b0;
-//        end
+
+always @ (posedge clkBTN or  posedge rst) begin // Sequential logic for the next state 
+    if(rst)
+        state <= CLK;
+    else
+        state <= nextState;
     end
-else begin
-        ENC=0;
-         visited=0;
-   end     
-//if (state==AS) begin
 
-//end
+reg ENC;  // Enable for the clock and alarm states
+reg ENTH, // Enable for the Time hour states   
+    ENTM, // Enable for the Time minute states
+    ENAM, // Enable for the Alarm mintue states
+    ENAH; // Enable for the alarm hour states
 
-//if(state==clk&A_count==count&!BTN)
-//begin 
-//clicked=1;
-//end
-//if(clicked)begin
-//LD0=0;
-//LD9=0;
-//end
-if(state==AS) begin visited=1;
-  LD0 = (A_count == count) ? clk_out1HZ : 1'b0;
-  LD9 = (A_count == count) ? clk_out1HZ : 1'b0;
+reg clkAdjst; // Clock for the counters
+
+reg BTN; 
+wire dec; // wire for the decrement 
+always @(*)
+    begin
+        BTN = (state==CLK&A_count==count&(U|D|R|L|C));
+        if(state==CLK) // Clock state
+        begin
+            {LD12,LD13,LD14,LD15}=5'b0000; 
+            ENC=en;
+            ENTH=0;
+            ENTM=0;
+            ENAM=0;
+            ENAH=0;
+            dp1=0;
+            LD0 = 0;
+            buzzer = 0;
+            dp1=1;
+        end
+        
+    else
+        visited=0;    
+    
+    if(state==AS) begin // Alarm state
+       visited=1;
+       LD0 = (A_count == count) ? clk_out1HZ : 1'b0;
+       buzzer = (A_count == count) ? clk_out1HZ : 1'b0;
+       dp1=1;
+       ENC=en;
+       ENTH=0;
+       ENTM=0;
+       ENAM=0;
+       ENAH=0;
+    end
+    
+    else if (state==TH) begin  // Time hour state
+       {LD0,LD12,LD13,LD14,LD15}=5'b11000;
+       ENTH=U|D;
+       dp1=0;
+       ENC=0;
+       ENTM=0;
+       ENAM=0;
+       ENAH=0;
+    end
+    
+    else if (state==TM) begin // Time minutes state
+       {LD0,LD12,LD13,LD14,LD15}=5'b10100;
+       ENTM=U|D;
+       dp1=0;
+       ENC=0;
+       ENAM=0;
+       ENAH=0;
+    end
+    
+    else if (state==AH) begin // Alarm hour state 
+       {LD0,LD12,LD13,LD14,LD15}=5'b10010; 
+       ENAH=U|D;
+       dp1=0;
+       ENC=0;
+       ENTM=0;
+       ENAM=0;
+    end
+    
+    else if (state==AM) begin // Alarm minutes state
+       {LD0,LD12,LD13,LD14,LD15}=5'b10001;
+       ENAM=U|D;
+       dp1=0;
+       ENC=0;
+       ENTM=0;
+       ENAH=0;
+    end
+    
+    if(state==CLK|state==AS) clkAdjst=clk_out1HZ; // Adjusting the clock for the counters based on the states
+    else clkAdjst = clkBTN;
 end
-else if (state==TH) begin
-{LD0,LD12,LD13,LD14,LD15}=5'b11000;
-ENTH=U|D;
-end
-else if (state==TM) begin 
-{LD0,LD12,LD13,LD14,LD15}=5'b10100;
-ENTM=U|D;
-end
-else if (state==AH)begin
-{LD0,LD12,LD13,LD14,LD15}=5'b10010;
-ENAH=U|D;
-end
-else if (state==AM) begin
-{LD0,LD12,LD13,LD14,LD15}=5'b10001;
-ENAM=U|D;
-end
 
-if(state==CLK) clkAdjst=clk_out1HZ;
-else clkAdjst = clkBTN;
-end
-assign dec = (ENTH & D) & (count[10:7]==0) & (count[13:11] ==0); 
-assign dec2 = (ENAH & D) & (A_count[10:7]==0) & (A_count[13:11] ==0);
-modulo10 secu( .clk(clkAdjst), .rst(rst),.en(ENC),.dec(dec),.D(D),.tens(0),.count(secount));
-modulo6 sect( .clk(clkAdjst), .rst(rst), .en((secount==9)&ENC),.dec(dec),.tens(0),.D(D),.count(secount2));
+assign dec = (ENTH & D) & (count[10:7]==0) & (count[13:11] ==0); // Decrement for the clock 
 
+assign dec2 = (ENAH & D) & (A_count[10:7]==0) & (A_count[13:11] ==0); // Decrement for the Alarm
 
+modulo10 secu( .clk(clkAdjst), .rst(rst),.en(ENC),.dec(dec),.D(D),.tens(0),.count(secount)); // Counter for the secounds units
 
+modulo6 sect( .clk(clkAdjst), .rst(rst), .en((secount==9)&ENC),.dec(dec),.tens(0),.D(D),.count(secount2)); // Counter for the secounds tens
 
-wire en_hours_mins;
+wire en_hours_mins; // enable for the hour minute counter 
+
 assign  en_hours_mins = ENC ?(secount==9)&(count[3:0]==9)&(secount2==5): count[3:0]==9 & ENTM&!D| (ENTM&(D&count[3:0]==0));
 
-modulo10 minu( .clk(clkAdjst), .rst(rst),.en(((secount==9)&(secount2==5)&ENC)|ENTM),.dec(dec),.tens(0),.D(D),.count(count[3:0]));
-modulo6 mint( .clk(clkAdjst), .rst(rst), .en(en_hours_mins),.dec(dec),.tens(0),.D(D),.count(count[6:4]));
+modulo10 minu( .clk(clkAdjst), .rst(rst),.en(((secount==9)&(secount2==5)&ENC)|ENTM),.dec(dec),.tens(0),.D(D),.count(count[3:0])); // Counter for the time minutes units
+modulo6 mint( .clk(clkAdjst), .rst(rst), .en(en_hours_mins),.dec(dec),.tens(0),.D(D),.count(count[6:4])); // Counter for the time minutes tens
 
 modulo10 hrsu( .clk(clkAdjst), .rst(rst|((count[10:7]==4)&(count [13:11]==2))),
- .en(ENC ? (count[6:4]==5)&(secount==9)&(count[3:0]==9)&(secount2==5):ENTH),.dec(dec),.tens(0),.D(D),.count(count[10:7]));
-wire en_hours_tens;
-assign  en_hours_tens = ENC ? count[10:7]==9&(count[6:4]==5)&(secount==9)&(count[3:0]==9)&(secount2==5): (count[10:7]==9 & ENTH &U) | (ENTH &(D&count[10:7]== 0));
-modulo_3 hrst( .clk(clkAdjst), .rst(rst|((count[10:7]==4)&(count [13:11]==2))), .en(en_hours_tens),.dec(0),.tens(1),.D(D),.count(count[13:11]));
+ .en(ENC ? (count[6:4]==5)&(secount==9)&(count[3:0]==9)&(secount2==5):ENTH),.dec(dec),.tens(0),.D(D),.count(count[10:7])); // Counter for the time hours units
+
+wire en_hours_tens; // enable for the hour tens counter
+
+assign  en_hours_tens = ENC ? count[10:7]==9&(count[6:4]==5)&(secount==9)&
+(count[3:0]==9)&(secount2==5): (count[10:7]==9 & ENTH &U) | (ENTH &(D&count[10:7]== 0));
+
+modulo_3 hrst( .clk(clkAdjst), .rst(rst|((count[10:7]==4)&(count [13:11]==2))), 
+.en(en_hours_tens),.dec(0),.tens(1),.D(D),.count(count[13:11]));  // Counter for the time hours tens
 
 
 
-modulo10 minuTO( .clk(clkAdjst), .rst(rst),.en(ENAM),.dec(dec2),.tens(0),.D(D),.count(A_count[3:0]));
-modulo6 mintO( .clk(clkAdjst), .rst(rst), .en(A_count[3:0]==9 & ENAM&!D| (ENAM&(D&A_count[3:0]==0))),.dec(dec2),.tens(0),.D(D),.count(A_count[6:4]));
+modulo10 minuTO( .clk(clkAdjst), .rst(rst),.en(ENAM),.dec(dec2),.tens(0),.D(D),.count(A_count[3:0])); // Counter for the alarm minutes units
+
+modulo6 mintO( .clk(clkAdjst), .rst(rst), .en(A_count[3:0]==9 & ENAM&!D | (ENAM&(D&A_count[3:0]==0))),
+.dec(dec2),.tens(0),.D(D),.count(A_count[6:4])); // Counter for the alarm hours tens
 
 modulo10 hrsuO( .clk(clkAdjst), .rst(rst|((A_count[10:7]==4)&(A_count [13:11]==2))),
- .en(ENAH),.dec(dec2),.tens(0),.D(D),.count(A_count[10:7]));
+ .en(ENAH),.dec(dec2),.tens(0),.D(D),.count(A_count[10:7])); // Counter for the alarm hours units
 
-modulo_3 hrstO( .clk(clkAdjst), .rst(rst|((A_count[10:7]==4)&(A_count [13:11]==2))), .en((A_count[10:7]==9 & ENAH &U) | (ENAH &(D&A_count[10:7]== 0))),.dec(0),.tens(1),.D(D),.count(A_count[13:11]));
+modulo_3 hrstO( .clk(clkAdjst), .rst(rst|((A_count[10:7]==4)&(A_count [13:11]==2))), .en((A_count[10:7]==9 & ENAH &U)
+ | (ENAH &(D&A_count[10:7]== 0))),.dec(0),.tens(1),.D(D),.count(A_count[13:11])); // Counter for the alarm hours tens
 
-//modulo10 A_minu( .clk(clk), .rst(rst), .en(ENAM),.count(A_count[3:0]),.dec(dec2),.tens(0),.D(D));
-//modulo6 A_mint( .clk(clk), .rst(rst), .en((A_count[3:0]==9)&ENAM),.dec(dec2),.count(A_count[6:4]),.D(D));
 
-//modulo10 A_hrsu( .clk(clk), .rst(rst|((A_count[10:7]==4)&(A_count [13:11]==2))),.dec(dec2), .en(ENAH),.count(A_count[10:7]),.D(D));
-//modulo_3 A_hrst( .clk(clk), .rst(rst|((A_count[10:7]==4)&(A_count [13:11]==2))),.dec(0),.en((A_count[10:7]==9&ENAH)),.D(D),.count(A_count[13:11]));
 
-assign MUX_count = (state == AH |state == AM) ? A_count : count;
-
+assign MUX_count = (state == AH |state == AM) ? A_count : count; // Choosing the count based on the state
 
 endmodule
